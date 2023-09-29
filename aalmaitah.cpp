8d5b6196890b909de49e78cd6e673e116722c6de
Alia Al-Maitah <113049867+AliaAlmaitah@users.noreply.github.com>
2023-09-29 09:02:06 -0700

    Update aalmaitah.cpp

--- aalmaitah.cpp
+++ aalmaitah.cpp
@@ -1,7 +1,7 @@
 /*
  *   Filename: aalmaitah.cpp
  *   Purpose: Software Engineering Project source file.
- *
+ *  (c) Copyright Alia Al-Maitah.
  *   Author: Alia Al-Maitah
  *   Created: January 1, 2023
  *   Last Modified:
