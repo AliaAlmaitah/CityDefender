2c2ee3a7280f5fd41996728033769b73e94a9837
Karen Santiago <ksantiago@odin.cs.csubak.edu>
2023-10-24 09:52:35 -0700

    total render calls

--- ksantiago.cpp
+++ ksantiago.cpp
@@ -6,6 +6,16 @@
 
 #include <GL/glx.h>
 #include "fonts.h"
+/*
+void display_startscreen() {
+    Rect r;
+    r.bot = (yres/2) - 20;
+    r.left = (xres/2) - 50;
+    r.center = 0;
+    ggprint40(&r, 0, 0x00ffffff, "City Defender");
+    r.bot -= 20;
+    ggprint12(&r, 0, 0x00ffffff, "Press 'P' to play.");
+}*/
 
 int total_render_function_calls(const bool get) {
     static int count = 0;
@@ -35,12 +45,11 @@ void display_credits(int xres, int yres)
     r.center = 0;
     ggprint16(&r, 0, 0x00ffffff, "Credits:");
     r.bot -= 20;
-    ggprint12(&r, 0, 0x00ffffff, "Alia Al-Maitah");
+    ggprint13(&r, 0, 0x00ffffff, "Alia Al-Maitah");
     r.bot -= 20;
-    ggprint12(&r, 0, 0x00ffffff, "Jayden Canales");
+    ggprint13(&r, 0, 0x00ffffff, "Jayden Canales");
     r.bot -= 20;
-    ggprint12(&r, 0, 0x00ffffff, "Bryan Estrada");
+    ggprint13(&r, 0, 0x00ffffff, "Bryan Estrada");
     r.bot -= 20;
-    ggprint12(&r, 0, 0x00ffffff, "Karen Santiago");
-    r.bot -= 20;
+    ggprint13(&r, 0, 0x00ffffff, "Karen Santiago");
 }
