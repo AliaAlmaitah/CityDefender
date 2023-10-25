5b726d0acb4e76e6c16effca3117a8d2e5e6d883
Alia Al-Maitah <aalmaitah@odin.cs.csubak.edu>
2023-10-24 20:25:30 -0700

    add key event

--- aalmaitah.cpp
+++ aalmaitah.cpp
@@ -6,3 +6,35 @@
  *   Created: January 1, 2023
  *   Last Modified:
 */
+
+#include <ctime>
+struct GLData {
+    bool statistics = false;
+    
+};
+
+GLData gl;
+int time_since_key_press(const bool get) {
+    static int firsttime = 1;
+    static int lastKeyPressTime;
+    if (firsttime) {
+        lastKeyPressTime = time(NULL);
+        firsttime = 0;
+    }
+    if (get) {
+        return time(NULL) - lastKeyPressTime;
+    }
+    return 0;
+}
+
+void updateKeyPressTime() {
+    time_since_key_press(false); // This resets the timer
+}
+/*void keyCallback(unsigned char key, int , int) {
+
+    if (key == 'S' || key == 's') {
+        gl.statistics = !gl.statistics;
+    } else {
+        updateKeyPressTime(); 
+    }
+}*/
