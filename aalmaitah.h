522f7ef18053bb1a3854f4d993282487da095ee9
Alia Al-Maitah <113049867+AliaAlmaitah@users.noreply.github.com>
2023-12-05 15:29:09 -0800

    Update aalmaitah.h

--- aalmaitah.h
+++ aalmaitah.h
@@ -1,26 +1,15 @@
 #ifndef _ALIA_H_
 #define _ALIA_H_
 
-// Alia's project file
+// Alia's project header file
 
-// Called from main program
-extern void display_name(int, int);
-extern void display_border(int xres, int yres);
-extern int time_since_key_press(const bool get);
-extern void handleKeyPressEvent();
-
-#ifdef USE_OPENAL_SOUND
-extern void playOpenALSample();
-//extern void toggleSound();  // Declaration for toggleSound
-//void cleanupOpenAL();
-//void playshootSound();
-
-#endif
-
-
-//class alia {
- //   int abc;
-//};
+// Function prototypes
+extern void display_name(int, int); // Display name function
+extern void display_border(int xres, int yres); // Display border function
+extern int time_since_key_press(const bool get); // Calculate time since key press
+extern void handleKeyPressEvent(); // Handle key press events
+void cleanupOpenAL(); // Clean up OpenAL resources
+void toggleSound(); // Toggle sound on/off
+void playShootSound(); // Play shooting sound effect
 
 #endif // _ALIA_H_
-
