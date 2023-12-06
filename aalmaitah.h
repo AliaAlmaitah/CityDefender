8457177abc85b3ebe10307769b6fab7ab5bbc796
Alia Al-Maitah <113049867+AliaAlmaitah@users.noreply.github.com>
2023-12-06 08:09:11 -0800

    Update aalmaitah.h

--- aalmaitah.h
+++ aalmaitah.h
@@ -1,15 +1,27 @@
 #ifndef _ALIA_H_
 #define _ALIA_H_
 
-// Alia's project header file
+// Include OpenAL headers
+#include <AL/al.h>
+#include <AL/alc.h>
+#include <AL/alut.h>
+
+// Extern declarations for global variables (if they are defined in another file)
+extern bool isSoundInitialized;
+extern ALuint alSource[2];
 
 // Function prototypes
 extern void display_name(int, int); // Display name function
 extern void display_border(int xres, int yres); // Display border function
 extern int time_since_key_press(const bool get); // Calculate time since key press
 extern void handleKeyPressEvent(); // Handle key press events
-void cleanupOpenAL(); // Clean up OpenAL resources
-void toggleSound(); // Toggle sound on/off
-void playShootSound(); // Play shooting sound effect
+extern void cleanupOpenAL(); // Clean up OpenAL resources
+extern void toggleSound(); // Toggle sound on/off
+extern void playShootSound(); // Play shooting sound effect
+
+// New function prototypes for advanced sound control
+void setVolume(ALuint sourceIndex, float volume); // Set volume of a sound source
+void setPitch(ALuint sourceIndex, float pitch); // Set pitch of a sound source
+void enableLooping(ALuint sourceIndex, bool shouldLoop); // Enable/disable looping for a sound source
 
 #endif // _ALIA_H_
